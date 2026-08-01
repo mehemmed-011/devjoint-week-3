989fa4d371496e91a37d7beda979392393eb7b00
mehemmed-011 <mehemmedibrahimli0090@gmail.com>
2026-08-01 22:02:21 +0400

    add resultList, Card resposive design

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,14 @@
 function Card() {
   return (
     <>
-      <h1>Card</h1>
+      <div className="card">
+        <div className="card__img--box">
+          <img className="card__img" src="https://m.media-amazon.com/images/M/MV5BYTU5ZTA5YTYtOWM3ZC00MDc5LWI4ZDAtNDVhNmJmYWVlZjhkXkEyXkFqcGdeQXVyODc2MjMzNjM@._V1_SX300.jpg" alt="Card Title" />
+        </div>
+        <h3 className="card__title">Dexter: The First Season - Witnessed in Blood: A True Murder Investigation</h3>
+        <p className="card__year">2014</p>
+        <span className="card__type">movie</span>
+      </div>
     </>
   );
 }
--- src/components/ResultsList.jsx
+++ src/components/ResultsList.jsx
@@ -3,7 +3,12 @@ import Card from "./Card";
 function ResultsList() {
   return (
     <>
-      
+      <div className="results-list">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </>
   );
 }
