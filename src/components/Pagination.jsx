422e59f3258c44f0c5da860caff609a4de06adb7
mehemmed-011 <mehemmedibrahimli0090@gmail.com>
2026-08-02 17:26:44 +0400

    add Pagination responsive design

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,7 +1,11 @@
 function Pagination() {
   return (
     <>
-      
+    <div className="pagination__box">
+        <button className="pagination__prev">🢀</button>
+        <p className="pagination__page">1/10</p>
+        <button className="pagination__next">🢂</button>
+      </div>
     </>
   );
 }
